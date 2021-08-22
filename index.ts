b0d73ed8b3b39f2b880844d3c487eead4ca6b9a7
Ken Cruz <ken@kencruz.ca>
2021-08-22 15:31:10 -0400

    implemented streetview api to return image

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import express, { Application, Request, Response } from "express";
+import fetch from "node-fetch";
 require("dotenv").config();
 
 const app: Application = express();
@@ -11,7 +12,29 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", async (req: Request, res: Response): Promise<Response> => {
   return res.status(200).send({
     message: "Hello World!",
+    streetViewUrl: process.env.GOOGLE_STREETVIEW_API,
+    openweather: process.env.OPENWEATHER_API,
+    query: req.query,
   });
+});
+
+// GET '/streetview?lat=<latitude>&long=<longitude>'
+app.get("/streetview", async (req: Request, res: Response): Promise<any> => {
+  const angle = Math.floor(Math.random() * 8) * 45;
+  const coords = { lat: req.query.lat, long: req.query.long };
+  const location = coords.lat + "," + coords.long;
+  const streetViewUrl = `https://maps.googleapis.com/maps/api/streetview?\
+size=640x320\
+&location=${location}\
+&key=${process.env.GOOGLE_STREETVIEW_API}\
+&heading=${angle}&pitch=-1.5`;
+
+  return fetch(streetViewUrl)
+    .then((r) => r.buffer())
+    .then((r) => {
+      res.set("Content-Type", "image/jpeg");
+      res.send(r);
+    });
 });
 
 try {
